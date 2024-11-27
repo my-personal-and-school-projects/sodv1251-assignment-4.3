b5769f2554e4200ce830d4103a5f43ba0f767af6
barrera77 <30675869+barrera77@users.noreply.github.com>
2024-11-27 01:57:21 -0700

    Added SQL Connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import mssql from "mssql";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+
+//create configuration for MSSQL
+const configMssql = {
+  user: process.env.MSSQL_USER_NAME,
+  password: process.env.MSSQL_PASSWORD,
+  database: "BVC_Airlines_DB",
+  server: "localhost",
+  options: {
+    encrypt: false,
+    trustedconnection: true,
+    enableArithAbort: true,
+  },
+};
+
+const poolPromise = mssql.connect(configMssql);
 
 // Serve static assets in production mode
 if (process.env.NODE_ENV === "production") {
